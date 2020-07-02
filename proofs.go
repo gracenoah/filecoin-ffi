cdda5e269afca49c4d47fca2d5485542cbf9309a
Erin Swenson-Healey <l@s3r.me>
2020-07-02 07:28:44 -0700

    add FauxRep to ffi package (#118)

--- proofs.go
+++ proofs.go
@@ -209,6 +209,24 @@ func FauxRep2(proofType abi.RegisteredSealProof, cacheDirPath string, existingPA
 	panic("")
 }
 
+func FauxRep(proofType abi.RegisteredSealProof, cacheDirPath string, sealedSectorPath string) (cid.Cid, error) {
+	sp, err := toFilRegisteredSealProof(proofType)
+	if err != nil {
+		return cid.Undef, err
+	}
+
+	resp := generated.FilFauxrep(sp, cacheDirPath, sealedSectorPath)
+	resp.Deref()
+
+	defer generated.FilDestroyFauxrepResponse(resp)
+
+	if resp.StatusCode != generated.FCPResponseStatusFCPNoError {
+		return cid.Undef, errors.New(generated.RawString(resp.ErrorMsg).Copy())
+	}
+
+	return commcid.ReplicaCommitmentV1ToCID(resp.Commitment[:]), nil
+}
+
 func toFilExistingPieceSizes(src []abi.UnpaddedPieceSize) ([]uint64, uint) {
 	panic("")
 }
