1d3d09bfc93f8885a08ffa63d047da0c71f81cc3
nemo <nemo@protocol.ai>
2020-10-16 16:06:24 -0400

    fix: move import statement

--- bls.go
+++ bls.go
@@ -4,6 +4,9 @@ package ffi
 // #cgo pkg-config: ${SRCDIR}/filcrypto.pc
 // #include "./filcrypto.h"
 import "C"
+import (
+	"github.com/filecoin-project/filecoin-ffi/generated"
+)
 
 // Hash computes the digest of a message
 func Hash(message Message) Digest {
